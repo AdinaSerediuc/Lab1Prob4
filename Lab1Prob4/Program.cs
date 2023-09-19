76780f0196563e2eb0e7364d606e6cd0a6111290
Ada Ababei <contact@adina_ababei@yahoo.com>
2023-09-19 19:19:14 +0300

    Ex4

--- Lab1Prob4/Program.cs
+++ Lab1Prob4/Program.cs
@@ -8,6 +8,29 @@ namespace Lab1Prob4
     {
         static void Main(string[] args)
         {
+
+            /*Ex 4
+Scrieti un program care va afisa semnul unui numar citit de la tastatura
+• Daca numarul este pozitiv, va afisa “+”
+• Daca numarul este negativ, va afisa “-”
+• Daca numarul este 0, va afisa “0”
+            */
+
+            int numar = int.Parse(Console.ReadLine);
+
+            if (numar < 0)
+                {
+                Console.WriteLine("Semnul numarului" + numar + " este -");
+            }
+            else if (numar = 0)
+                {
+                Console.WriteLine("Numarul nu are semn" + numar);
+            }
+            else if (numar > 0)
+                {
+                Console.WriteLine("Semnul numarului" + numar + "este +");
+            }
+
         }
     }
 }
